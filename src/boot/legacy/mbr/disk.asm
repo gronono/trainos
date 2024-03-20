77df3fbcec9a4025a7d2ac086f0af0919eb0a59d
Arnaud Brunet <gronono@users.noreply.github.com>
2024-03-20 15:32:31 +1100

    docs

--- src/boot/legacy/mbr/disk.asm
+++ src/boot/legacy/mbr/disk.asm
@@ -1,11 +1,15 @@
 ; Load Volume Boot Record
 load_vbr:
-    ; Fill DAP
+    ; Compute the LBA address
+    ; = FIRST_BOOT_FLAG_ADDR + (selected * PARTITION_ENTRY_SIZE) + LBA_ADDR_OFFSET
+    ; = 0x1BE + (selected * 10) + 0x08
+    ; for the 1st partition : 0x1BE + 0x08 =  0x01C6
+    ; should be 0x800 = 2048th sector (because parted alignment)
+    ; => located at 0x800 * (size of a sector = 512 = 0x200) = 0x100000
     mov bx, [selected_menu]
     mov eax, PARTITION_ENTRY_SIZE
     mul bx
-    add eax, FIRST_BOOT_FLAG_ADDR
-    add eax, LBA_ADDR_OFFSET
+    add eax, (FIRST_BOOT_FLAG_ADDR + LBA_ADDR_OFFSET)
     mov ebx, eax
     mov eax, dword [ds:ebx]
     mov dword [disk_dap.lba], eax
@@ -36,4 +40,5 @@ disk_dap:
     ; destination = 0x0000:0x7C00
     .offset     dw  0x7C00
     .segment    dw  0x0000
+    ; LBA Address = index (zero based) of the first sector to read
     .lba        dq  0x00000000
