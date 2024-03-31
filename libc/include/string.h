85838391ae49a9c38097f5613781a82b0958ffb3
Arnaud Brunet <gronono@users.noreply.github.com>
2024-03-31 23:45:10 +1100

    add strcmp

--- libc/include/string.h
+++ libc/include/string.h
@@ -4,5 +4,5 @@
 
 /** https://en.cppreference.com/w/c/string/byte/strlen */
 size_t strlen(const char *str);
-
+int strcmp(const char* str1, const char* str2);
 void strrev(char* str);
--- libc/include/sys/param.h
+++ libc/include/sys/param.h
@@ -1,4 +1,6 @@
 #pragma once
 
+#define BREAKPOINT __asm__ volatile("xchgw %bx, %bx");
+
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
